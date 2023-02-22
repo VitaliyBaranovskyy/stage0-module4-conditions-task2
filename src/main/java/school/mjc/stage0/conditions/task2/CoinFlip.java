257a74fa3431828e91435386312f9b25a90a0f1f
VitaliyBaranovskyy <40470956+VitaliyBaranovskyy@users.noreply.github.com>
2023-02-22 15:51:52 +0200

    Update CoinFlip.java

--- src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
+++ src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
@@ -2,6 +2,18 @@ package school.mjc.stage0.conditions.task2;
 
 public class CoinFlip {
     public void throwCoin(int from1UpTo1000) {
+        
+        if (from1UpTo1000 <= 500)
+            System.out.println("Eagle");
+        
+        if (from1UpTo1000 > 500 && from1UpTo1000 <= 1000)
+            System.out.println("Tail");
 
+    }
+    
+    public static void main(String args[]){
+     CoinFlip main = new CoinFlip();
+        main.throwCoin(750);
+        
     }
 }
