46dfcf543b8cec6c6efe8000668631f3c0d2886a
VitaliyBaranovskyy <40470956+VitaliyBaranovskyy@users.noreply.github.com>
2023-02-22 15:46:25 +0200

    Update GreatestNumberPrinter.java

--- src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
@@ -2,5 +2,18 @@ package school.mjc.stage0.conditions.task2;
 
 public class GreatestNumberPrinter {
     public void printGreatest(int first, int second) {
+        if (first > second)
+            System.out.println(first);
+        
+        if (first == second)
+            System.out.println(first | second);
+        
+        if (second > first)
+            System.out.println(second);
+    }
+    
+    public static void main(String args[]) {
+       GreatestNumberPrinter main = new GreatestNumberPrinter();
+        main.printGreatest(55,65);
     }
 }
